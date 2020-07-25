ef146eeb647199270eca353a2c4047b6a49f965e
vishal karhade <sarita34karhade@gmail.com>
2020-07-25 16:42:56 +0530

    first commit

---
+++ weather/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+import requests
+
+# Create your views here.
+def index(request):
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=?';
+    city="Indore"
+    if request.method=="POST":
+        city=request.POST['city']
+    print("Vishal")
+    print(city)
+    r=requests.get(url.format(city)).json();
+    city_temp={
+        'city':city,
+        'logo':r['weather'][0]['icon'],
+        'temp':r['main']['temp'],
+        'pressure':r['main']['pressure'],
+        'humidity':r['main']['humidity'],
+        'desc':r['weather'][0]['description']
+    }
+    context={'city_temp':city_temp}
+    print(r)
+    return render(request,'weather/weather.html',context)
+
+def add(render):
+    print("Hello World!")
